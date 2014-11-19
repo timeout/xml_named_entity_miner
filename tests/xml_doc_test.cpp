401276d327b58a40db741de7ee6f0eec8962b9d0
Joe Gain <joe.gain@gmail.com>
2014-11-19 21:12:59 +0100

    added operator bool as test for non-null initilisation

--- tests/xml_doc_test.cpp
+++ tests/xml_doc_test.cpp
@@ -17,12 +17,14 @@ static const Pathname buildDir_{Dir::getInstance( )->pwd( )};
 class XmlDocTests : public ::testing::Test {
 protected:
     virtual auto SetUp( ) -> void { Dir::getInstance( )->chdir( buildDir_ ); }
+    auto testable( const XmlDoc &xml ) const -> bool { return xml ? true : false; }
 };
 
 TEST_F( XmlDocTests, DefaultCtor ) {
     const XmlDoc xml;
     EXPECT_FALSE( xml.errorHandler( ).hasErrors( ) );
-    EXPECT_STREQ( "", xml.toString( ).c_str() );
+    EXPECT_STREQ( "", xml.toString( ).c_str( ) );
+    ASSERT_EQ( false, testable( xml ) );
 }
 
 TEST_F( XmlDocTests, ValidPathnameConstructor ) {
@@ -53,6 +55,8 @@ TEST_F( XmlDocTests, ValidPathnameConstructor ) {
             std::cerr << p << std::endl;
             std::cerr << xml.errorHandler( ) << std::endl;
         }
+        EXPECT_FALSE( !xml );
+        ASSERT_EQ( true, testable( xml ) );
     }
 }
 
