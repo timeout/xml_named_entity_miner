d08e509f19b76a0be54c61921ba7c9ae449642d3
Joe Gain <joe.gain@gmail.com>
2014-11-21 10:46:50 +0100

    initial commit

---
+++ lib/xpath_ctxt.hpp
@@ -0,0 +1,56 @@
+#pragma once
+
+#include "xml_doc.hpp"
+#include "libxml2_error_handlers.hpp"
+#include <libxml/xpath.h>
+#include <memory>
+
+class FreeXPathCtxt {
+public:
+    auto operator( )( xmlXPathContext *xpathCtxt ) const -> void {
+        xmlXPathFreeContext( xpathCtxt );
+    }
+};
+
+class XPathCtxt {
+    friend class XPathQuery;
+
+public:
+    XPathCtxt( ) : xpathCtxt_{nullptr} {}
+    explicit XPathCtxt( const XmlDoc &xml )
+        : xpathCtxt_{xmlXPathNewContext( xml.xmlDoc_.get( ) )} {
+        xpathHandler_.registerHandler( xpathCtxt_.get( ) );
+    }
+    XPathCtxt( const XPathCtxt &xpathCtxt ) {
+        xpathCtxt_.reset( xmlXPathNewContext( xpathCtxt.xpathCtxt_->doc ) );
+        xpathHandler_.registerHandler( xpathCtxt_.get( ) );
+    }
+    XPathCtxt( XPathCtxt &&xpathCtxt )
+        : xpathCtxt_{std::move( xpathCtxt.xpathCtxt_ )},
+          xpathHandler_{std::move( xpathCtxt.xpathHandler_ )} {}
+    auto operator=( const XPathCtxt &xpathCtxt ) -> XPathCtxt & {
+        XPathCtxt tmp( xpathCtxt );
+        xpathCtxt_ = std::move( tmp.xpathCtxt_ );
+        xpathHandler_.registerHandler( xpathCtxt_.get( ) );
+        return *this;
+    }
+    auto operator=( XPathCtxt &&xpathCtxt ) -> XPathCtxt & {
+        xpathCtxt_ = std::move( xpathCtxt.xpathCtxt_ );
+        xpathHandler_ = std::move( xpathCtxt.xpathHandler_ );
+        return *this;
+    }
+    explicit operator bool( ) const { return ( xpathCtxt_ != nullptr ); }
+    friend auto operator>>( const XmlDoc &xml, XPathCtxt &xpathCtxt ) -> XPathCtxt & {
+        xpathCtxt.xpathCtxt_.reset( xmlXPathNewContext( xml.xmlDoc_.get( ) ) );
+        xpathCtxt.xpathHandler_.registerHandler(
+            xpathCtxt.xpathCtxt_.get( ) ); // ist this correct?
+        return xpathCtxt;
+    }
+    auto errorHandler( ) const -> const IErrorHandler & { return xpathHandler_; }
+
+private:
+    using XPathCtxtT = std::unique_ptr<xmlXPathContext, FreeXPathCtxt>;
+    XPathCtxtT xpathCtxt_;
+    XPathErrorHandler xpathHandler_;
+};
+
