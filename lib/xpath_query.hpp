2d9253844f8e584e65f03e2f8f28ee109a530f86
Joe Gain <joe.gain@gmail.com>
2014-11-26 23:50:11 +0100

    added move constructors and a move assignment operator

--- lib/xpath_query.hpp
+++ lib/xpath_query.hpp
@@ -1,5 +1,8 @@
 #pragma once
+
 #include "xpath_ctxt.hpp"
+// #include "xpath_result_set.hpp"
+
 #include <vector>
 
 class FreeXPathQuery {
@@ -13,8 +16,31 @@ class XPathQuery {
 public:
     XPathQuery( ) : query_{nullptr} {}
     explicit XPathQuery( const XPathCtxt &ctxt ) : query_{nullptr}, queryCtxt_{ctxt} {}
-    XPathQuery( const XPathQuery & ) = delete;
-    auto operator=( const XPathQuery & ) -> XPathQuery & = delete;
+    XPathQuery( const XPathQuery &query ) : queryCtxt_{query.queryCtxt_} {
+        if ( query.query_ ) {
+            query_.reset( xmlXPathObjectCopy( query.query_.get( ) ) );
+        } else {
+            query_.reset( nullptr );
+        }
+    }
+    XPathQuery( XPathQuery &&query )
+        : query_{std::move( query.query_ )}, queryCtxt_{std::move( query.queryCtxt_ )} {}
+    auto operator=( const XPathQuery &rhs ) -> XPathQuery & {
+        if ( this != &rhs ) {
+            queryCtxt_ = rhs.queryCtxt_;
+            if ( rhs.query_ ) {
+                query_.reset( xmlXPathObjectCopy( rhs.query_.get( ) ) );
+            } else {
+                query_.reset( nullptr );
+            }
+        }
+        return *this;
+    }
+    auto operator=( XPathQuery &&rhs ) -> XPathQuery & {
+        query_ = std::move( rhs.query_ );
+        queryCtxt_ = std::move( rhs.queryCtxt_ );
+        return *this;
+    }
     friend auto operator>>( const XPathCtxt &xpctxt, XPathQuery &query ) -> XPathQuery & {
         query.queryCtxt_ = xpctxt;
         return query;
@@ -27,11 +53,15 @@ public:
                 query_.reset(
                     xmlXPathEvalExpression( expr, queryCtxt_.xpathCtxt_.get( ) ) );
             } else {
-                query_ = std::move( XPathQueryT{
-                    xmlXPathEvalExpression( expr, queryCtxt_.xpathCtxt_.get( ) )} );
+                // query_ = std::move( XPathQueryT{
+                //     xmlXPathEvalExpression( expr, queryCtxt_.xpathCtxt_.get( ) )} );
+                XPathQueryT tmp{
+                    xmlXPathEvalExpression( expr, queryCtxt_.xpathCtxt_.get( ) )};
+                query_.swap( tmp );
             }
         }
     }
+    // auto process( ) -> XPathResultSet {}
 
 private:
     using XPathQueryT = std::unique_ptr<xmlXPathObject, FreeXPathQuery>;
