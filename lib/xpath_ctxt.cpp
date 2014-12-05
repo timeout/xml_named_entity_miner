c0ae7ba543b8151abea389d1efdfb2c143104993
Joe Gain <joe.gain@gmail.com>
2014-12-05 19:47:26 +0100

    split into header and implementation

---
+++ lib/xpath_ctxt.cpp
@@ -0,0 +1,46 @@
+#include "xpath_ctxt.hpp"
+#include "xpath_query.hpp"
+#include <iostream>
+
+auto FreeXPathCtxt::operator( )( xmlXPathContext *xpathCtxt ) const -> void {
+    xmlXPathFreeContext( xpathCtxt );
+}
+
+XPathCtxt::XPathCtxt( ) : xpathCtxt_{nullptr} {}
+XPathCtxt::XPathCtxt( const XmlDoc &xml )
+    : xpathCtxt_{xmlXPathNewContext( xml.xmlDoc_.get( ) )}, xml_{xml} {
+    xpathHandler_.registerHandler( xpathCtxt_.get( ) );
+}
+XPathCtxt::XPathCtxt( const XPathCtxt &xpathCtxt ) : xml_{xpathCtxt.xml_} {
+    if ( xpathCtxt ) {
+        xpathCtxt_.reset( xmlXPathNewContext( xpathCtxt.xml_.xmlDoc_.get( ) ) );
+        xpathHandler_.registerHandler( xpathCtxt_.get( ) );
+    }
+}
+XPathCtxt::XPathCtxt( XPathCtxt &&xpathCtxt )
+    : xpathCtxt_{std::move( xpathCtxt.xpathCtxt_ )}, xml_{std::move( xpathCtxt.xml_ )},
+      xpathHandler_{std::move( xpathCtxt.xpathHandler_ )} {}
+auto XPathCtxt::operator=( const XPathCtxt &rhs ) -> XPathCtxt & {
+    if ( this != &rhs ) {
+        xml_ = rhs.xml_;
+        if ( rhs ) {
+            xpathCtxt_.reset( xmlXPathNewContext( rhs.xml_.xmlDoc_.get( ) ) );
+            xpathHandler_ = rhs.xpathHandler_;
+        } else {
+            xpathCtxt_.reset( nullptr );
+        }
+    }
+    return *this;
+}
+auto XPathCtxt::operator=( XPathCtxt &&xpathCtxt ) -> XPathCtxt & {
+    xml_ = std::move( xpathCtxt.xml_ );
+    xpathCtxt_ = std::move( xpathCtxt.xpathCtxt_ );
+    xpathHandler_ = std::move( xpathCtxt.xpathHandler_ );
+    return *this;
+}
+XPathCtxt::operator bool( ) const {
+    return ( xpathCtxt_ != nullptr && xpathCtxt_->doc != nullptr );
+}
+auto XPathCtxt::errorHandler( ) const -> const IErrorHandler & { return xpathHandler_; }
+auto XPathCtxt::makeQuery( ) const -> const XPathQuery { return XPathQuery{*this}; }
+
--- lib/xpath_ctxt.hpp
+++ lib/xpath_ctxt.hpp
@@ -4,63 +4,28 @@
 #include "libxml2_error_handlers.hpp"
 #include <libxml/xpath.h>
 #include <memory>
-#include <iostream>
+
+class XPathQuery;
 
 class FreeXPathCtxt {
 public:
-    auto operator( )( xmlXPathContext *xpathCtxt ) const -> void {
-        xmlXPathFreeContext( xpathCtxt );
-    }
+    auto operator( )( xmlXPathContext *xpathCtxt ) const -> void;
 };
 
 class XPathCtxt {
     friend class XPathQuery;
 
 public:
-    XPathCtxt( ) : xpathCtxt_{nullptr} {}
-    explicit XPathCtxt( const XmlDoc &xml )
-        : xpathCtxt_{xmlXPathNewContext( xml.xmlDoc_.get( ) )}, xml_{xml} {
-        xpathHandler_.registerHandler( xpathCtxt_.get( ) );
-    }
-    XPathCtxt( const XPathCtxt &xpathCtxt ) : xml_{xpathCtxt.xml_} {
-        if ( xpathCtxt ) {
-            xpathCtxt_.reset( xmlXPathNewContext( xpathCtxt.xml_.xmlDoc_.get( ) ) );
-            xpathHandler_.registerHandler( xpathCtxt_.get( ) );
-        }
-    }
-    XPathCtxt( XPathCtxt &&xpathCtxt )
-        : xpathCtxt_{std::move( xpathCtxt.xpathCtxt_ )},
-          xml_{std::move( xpathCtxt.xml_ )},
-          xpathHandler_{std::move( xpathCtxt.xpathHandler_ )} {}
-    auto operator=( const XPathCtxt &rhs ) -> XPathCtxt & {
-        if ( this != &rhs ) {
-            xml_ = rhs.xml_;
-            if ( rhs ) {
-                xpathCtxt_.reset( xmlXPathNewContext( rhs.xml_.xmlDoc_.get( ) ) );
-                xpathHandler_ = rhs.xpathHandler_;
-            } else {
-                xpathCtxt_.reset( nullptr );
-            }
-        }
-        return *this;
-    }
-    auto operator=( XPathCtxt &&xpathCtxt ) -> XPathCtxt & {
-        xml_ = std::move( xpathCtxt.xml_ );
-        xpathCtxt_ = std::move( xpathCtxt.xpathCtxt_ );
-        xpathHandler_ = std::move( xpathCtxt.xpathHandler_ );
-        return *this;
-    }
-    explicit operator bool( ) const {
-        return ( xpathCtxt_ != nullptr && xpathCtxt_->doc != nullptr );
-    }
-    friend auto operator>>( const XmlDoc &xml, XPathCtxt &xpathCtxt ) -> XPathCtxt & {
-        xpathCtxt.xml_ = xml;
-        xpathCtxt.xpathCtxt_.reset( xmlXPathNewContext( xpathCtxt.xml_.xmlDoc_.get( ) ) );
-        xpathCtxt.xpathHandler_.registerHandler(
-            xpathCtxt.xpathCtxt_.get( ) ); // ist this correct?
-        return xpathCtxt;
-    }
-    auto errorHandler( ) const -> const IErrorHandler & { return xpathHandler_; }
+    XPathCtxt( );
+    explicit XPathCtxt( const XmlDoc &xml );
+    XPathCtxt( const XPathCtxt &xpathCtxt );
+    XPathCtxt( XPathCtxt &&xpathCtxt );
+    auto operator=( const XPathCtxt &rhs ) -> XPathCtxt &;
+    auto operator=( XPathCtxt &&xpathCtxt ) -> XPathCtxt &;
+    explicit operator bool( ) const;
+    friend auto operator>>( const XmlDoc &xml, XPathCtxt &xpathCtxt ) -> XPathCtxt &;
+    auto errorHandler( ) const -> const IErrorHandler &;
+    auto makeQuery() const -> const XPathQuery;
 
 private:
     using XPathCtxtT = std::unique_ptr<xmlXPathContext, FreeXPathCtxt>;
@@ -68,4 +33,11 @@ private:
     XmlDoc xml_;
     XPathErrorHandler xpathHandler_;
 };
+
+inline auto operator>>( const XmlDoc &xml, XPathCtxt &xpathCtxt ) -> XPathCtxt & {
+    xpathCtxt.xml_ = xml;
+    xpathCtxt.xpathCtxt_.reset( xmlXPathNewContext( xpathCtxt.xml_.get( ) ) );
+    xpathCtxt.xpathHandler_.registerHandler( xpathCtxt.xpathCtxt_.get( ) );
+    return xpathCtxt;
+}
 
