e99fe3b5b9a8494c3327ae351f3df494167fdb07
Joe Gain <joe.gain@gmail.com>
2014-12-05 19:47:44 +0100

    split into header and implementation

---
+++ lib/xpath_query.cpp
@@ -0,0 +1,47 @@
+#include "xpath_query.hpp"
+
+auto FreeXPathQuery::operator( )( xmlXPathObject *queryObject ) const -> void {
+    xmlXPathFreeObject( queryObject );
+};
+
+XPathQuery::XPathQuery( ) : query_{nullptr} {}
+XPathQuery::XPathQuery( const XPathCtxt &ctxt )
+    : query_{nullptr}, queryCtxt_{ctxt} {}
+XPathQuery::XPathQuery( const XPathQuery &query ) : queryCtxt_{query.queryCtxt_} {
+    if ( query.query_ ) {
+        query_.reset( xmlXPathObjectCopy( query.query_.get( ) ) );
+    } else {
+        query_.reset( nullptr );
+    }
+}
+XPathQuery::XPathQuery( XPathQuery &&query )
+    : query_{std::move( query.query_ )}, queryCtxt_{std::move( query.queryCtxt_ )} {}
+auto XPathQuery::operator=( const XPathQuery &rhs ) -> XPathQuery & {
+    if ( this != &rhs ) {
+        queryCtxt_ = rhs.queryCtxt_;
+        if ( rhs.query_ ) {
+            query_.reset( xmlXPathObjectCopy( rhs.query_.get( ) ) );
+        } else {
+            query_.reset( nullptr );
+        }
+    }
+    return *this;
+}
+auto XPathQuery::operator=( XPathQuery &&rhs ) -> XPathQuery & {
+    query_ = std::move( rhs.query_ );
+    queryCtxt_ = std::move( rhs.queryCtxt_ );
+    return *this;
+}
+XPathQuery::operator bool( ) const { return ( query_ != nullptr ); }
+auto XPathQuery::query( const std::string &xpath ) -> void {
+    if ( queryCtxt_ ) {
+        const xmlChar *expr = ( xmlChar * )xpath.c_str( );
+        if ( query_ ) {
+            query_.reset( xmlXPathEvalExpression( expr, queryCtxt_.xpathCtxt_.get( ) ) );
+        } else {
+            XPathQueryT tmp{xmlXPathEvalExpression( expr, queryCtxt_.xpathCtxt_.get( ) )};
+            query_ = std::move( tmp );
+        }
+    }
+}
+
--- lib/xpath_query.hpp
+++ lib/xpath_query.hpp
@@ -1,67 +1,25 @@
 #pragma once
 
 #include "xpath_ctxt.hpp"
-// #include "xpath_result_set.hpp"
-
-#include <vector>
-#include <iostream>
 
 class FreeXPathQuery {
 public:
-    auto operator( )( xmlXPathObject *queryObject ) const -> void {
-        xmlXPathFreeObject( queryObject );
-    };
+    auto operator( )( xmlXPathObject *queryObject ) const -> void;
 };
 
 class XPathQuery {
     friend class XPathResultSet;
 
 public:
-    XPathQuery( ) : query_{nullptr} {}
-    explicit XPathQuery( const XPathCtxt &ctxt ) : query_{nullptr}, queryCtxt_{ctxt} {}
-    XPathQuery( const XPathQuery &query ) : queryCtxt_{query.queryCtxt_} {
-        if ( query.query_ ) {
-            query_.reset( xmlXPathObjectCopy( query.query_.get( ) ) );
-        } else {
-            query_.reset( nullptr );
-        }
-    }
-    XPathQuery( XPathQuery &&query )
-        : query_{std::move( query.query_ )}, queryCtxt_{std::move( query.queryCtxt_ )} {}
-    auto operator=( const XPathQuery &rhs ) -> XPathQuery & {
-        if ( this != &rhs ) {
-            queryCtxt_ = rhs.queryCtxt_;
-            if ( rhs.query_ ) {
-                query_.reset( xmlXPathObjectCopy( rhs.query_.get( ) ) );
-            } else {
-                query_.reset( nullptr );
-            }
-        }
-        return *this;
-    }
-    auto operator=( XPathQuery &&rhs ) -> XPathQuery & {
-        query_ = std::move( rhs.query_ );
-        queryCtxt_ = std::move( rhs.queryCtxt_ );
-        return *this;
-    }
-    friend auto operator>>( const XPathCtxt &xpctxt, XPathQuery &query ) -> XPathQuery & {
-        query.queryCtxt_ = xpctxt;
-        return query;
-    }
-    explicit operator bool( ) const { return ( query_ != nullptr ); }
-    auto query( const std::string &xpath ) -> void {
-        if ( queryCtxt_ ) {
-            const xmlChar *expr = ( xmlChar * )xpath.c_str( );
-            if ( query_ ) {
-                query_.reset(
-                    xmlXPathEvalExpression( expr, queryCtxt_.xpathCtxt_.get( ) ) );
-            } else {
-                XPathQueryT tmp{
-                    xmlXPathEvalExpression( expr, queryCtxt_.xpathCtxt_.get( ) )};
-                query_ = std::move( tmp );
-            }
-        }
-    }
+    XPathQuery( );
+    explicit XPathQuery( const XPathCtxt &ctxt );
+    XPathQuery( const XPathQuery &query );
+    XPathQuery( XPathQuery &&query );
+    auto operator=( const XPathQuery &rhs ) -> XPathQuery & ;
+    auto operator=( XPathQuery &&rhs ) -> XPathQuery &;
+    friend auto operator>>( const XPathCtxt &xpctxt, XPathQuery &query ) -> XPathQuery &;
+    explicit operator bool( ) const;
+    auto query( const std::string &xpath ) -> void;
     // auto process( ) -> XPathResultSet {}
 
 private:
@@ -69,3 +27,9 @@ private:
     XPathQueryT query_;
     XPathCtxt queryCtxt_;
 };
+
+inline auto operator>>( const XPathCtxt &xpctxt, XPathQuery &query ) -> XPathQuery & {
+    query.queryCtxt_ = xpctxt;
+    return query;
+}
+
