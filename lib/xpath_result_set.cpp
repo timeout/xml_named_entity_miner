27eda672fe8306b47716d6f84b823e75c33cbe0a
Joe Gain <joe.gain@gmail.com>
2014-12-14 13:44:33 +0100

    refactored and rewritten incl. split into header and impl

---
+++ lib/xpath_result_set.cpp
@@ -0,0 +1,68 @@
+#include "xpath_result_set.hpp"
+#include "libxml/xpath.h"
+
+XPathResultSet::XPathResultSet( ) : query_{}, resultSet_{nullptr}, sz_{0}, idx_{0} {}
+
+XPathResultSet::XPathResultSet( const XPathQuery &query ) : query_{query} {
+    if ( query_ && query_.query_->nodesetval ) {
+        resultSet_ = query_.query_->nodesetval;
+        sz_ = resultSet_->nodeNr;
+    } else {
+        resultSet_ = nullptr;
+        sz_ = 0;
+    }
+    idx_ = 0;
+}
+
+XPathResultSet::XPathResultSet( const XPathResultSet &other ) : query_{other.query_} {
+    if ( query_ && query_.query_->nodesetval ) {
+        resultSet_ = query_.query_->nodesetval;
+        sz_ = resultSet_->nodeNr;
+    } else {
+        resultSet_ = nullptr;
+        sz_ = 0;
+    }
+    idx_ = other.idx_;
+}
+
+XPathResultSet::XPathResultSet( XPathResultSet &&rhs )
+    : query_{std::move( rhs.query_ )}, resultSet_{rhs.resultSet_}, sz_{rhs.sz_},
+      idx_{rhs.idx_} {}
+
+auto XPathResultSet::operator=( const XPathResultSet &rhs ) -> XPathResultSet & {
+    if ( this != &rhs ) {
+        query_ = rhs.query_;
+        if ( query_ && query_.query_->nodesetval ) {
+            resultSet_ = query_.query_->nodesetval;
+            sz_ = resultSet_->nodeNr;
+        } else {
+            resultSet_ = nullptr;
+            sz_ = 0;
+        }
+        idx_ = rhs.idx_;
+    }
+    return *this;
+}
+auto XPathResultSet::operator=( XPathResultSet &&rhs ) -> XPathResultSet & {
+    query_ = std::move( rhs.query_ );
+    resultSet_ = rhs.resultSet_;
+    sz_ = rhs.sz_;
+    idx_ = rhs.idx_;
+    return *this;
+}
+
+auto XPathResultSet::operator[]( size_t idx ) -> XmlNode {
+    if ( resultSet_ && ( idx_ <= sz_ ) ) {
+        idx_ = idx;
+        return XmlNode{resultSet_->nodeTab[idx_]};
+    }
+    return XmlNode{};
+}
+
+// private:
+//     XPathQuery query_;
+//     xmlNodeSetPtr resultSet_;
+//     size_t sz_;
+//     size_t idx_;
+// };
+
