b7c686b29ef075fa179c79ef85796b17de651332
Joe Gain <joe.gain@gmail.com>
2015-01-12 14:37:30 +0100

    split mainwindow into header and implementation files

--- gui/mainwindow.hpp
+++ gui/mainwindow.hpp
@@ -1,4 +1,5 @@
 #pragma once
+#include "xml_display.hpp"
 #include "xml_tree.hpp"
 #include "xml_doc.hpp"
 
@@ -9,6 +10,8 @@
 #include <QAction>
 #include <QFileDialog>
 #include <QPoint>
+#include <QDockWidget>
+#include <QStatusBar>
 
 #include <string>
 #include <fstream>
@@ -16,57 +19,20 @@
 class Mainwindow : public QMainWindow {
     Q_OBJECT
 public:
-    Mainwindow( QWidget *parent = nullptr )
-        : QMainWindow{parent}, xml_{}, tr_{new XmlTree} {
-
-        createActions( );
-        createMenus( );
-        setCentralWidget( tr_ );
-
-        // context menu for tree view
-        connect( tr_, SIGNAL( customContextMenuRequested( const QPoint & )), this,
-                 SLOT( onCustomContextRequest( const QPoint & )) );
-    }
+    Mainwindow( QWidget *parent = nullptr );
 
 public slots:
-    void open( ) {
-        const QString filename = QFileDialog::getOpenFileName(
-            this, tr( "Open XML file" ), "/usr/local/home/joe",
-            tr( "Xml Files (*.xml)" ) );
-        if ( !filename.isEmpty( ) ) {
-            std::ifstream in{filename.toUtf8( ).constData( )};
-            xml_ = XmlDoc{in};
-            tr_->xml( xml_ );
-        }
-    }
-    void onCustomContextRequest( const QPoint &pos ) {
-        QModelIndex idx = tr_->indexAt( pos );
-        if ( idx.isValid( ) ) {
-            xmlTreeContextMenu_->exec( tr_->mapToGlobal( pos ) );
-        }
-    }
+    void open( );
+    void onCustomContextRequest( const QPoint &pos );
 
 private:
-    void createMenus( ) {
-        QMenu *fileMenu = menuBar( )->addMenu( tr( "&File" ) );
-        fileMenu->addAction( openAct_ );
-
-        xmlTreeContextMenu_ = new QMenu( this );
-        // TODO: create actions: select all, clear selections?
-        // stub
-        xmlTreeContextMenu_->addAction( openAct_ );
-    }
-
-    void createActions( ) {
-        openAct_ = new QAction{tr( "&Open" ), this};
-        openAct_->setShortcuts( QKeySequence::Open );
-        openAct_->setStatusTip( tr( "Open an existing file" ) );
-        connect( openAct_, SIGNAL( triggered( ) ), this, SLOT( open( ) ) );
-    }
+    void createMenus( );
+    void createActions( );
 
     XmlDoc xml_;
     QAction *openAct_;
-    XmlTree *tr_;
+    XmlDisplay *xmlDisplay_;
+    XmlTree *xmlNavigator_;
     QMenu *xmlTreeContextMenu_;
 };
 
