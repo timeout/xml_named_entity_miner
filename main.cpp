c7de4bec3aa7331723368178fcd3428684036b99
Joe Gain <joe.gain@gmail.com>
2015-02-23 22:38:45 +0100

    added global app settings to main class

--- main.cpp
+++ main.cpp
@@ -5,6 +5,8 @@
 
 int main( int argc, char *argv[] ) {
     QApplication app( argc, argv );
+    app.setOrganizationName("University-of-Konstanz");
+    app.setApplicationName("Named-Entity-Miner");
     // Mainwindow win;
     MainWindow win;
     win.show( );
