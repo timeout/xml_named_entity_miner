33ced33a29693f2c427f5a2e22ada206cf6a39bc
Joe Gain <joe.gain@gmail.com>
2015-02-23 16:17:12 +0100

    removed the old gui from the CMakefiles

--- main.cpp
+++ main.cpp
@@ -1,10 +1,12 @@
 #include "lib/bilanz.hpp"
-#include "gui/mainwindow.hpp"
+// #include "gui/mainwindow.hpp"
+#include "gui2/mainwindow.hpp"
 #include <QApplication>
 
 int main( int argc, char *argv[] ) {
     QApplication app( argc, argv );
-    Mainwindow win;
+    // Mainwindow win;
+    MainWindow win;
     win.show( );
     return app.exec( );
 }
