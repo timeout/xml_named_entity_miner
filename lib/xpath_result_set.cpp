3020485533cf3b19f5bf18967a0fa75fc3e5b139
Joe Gain <joe.gain@gmail.com>
2015-02-26 01:03:27 +0100

    added jury rigged iterators ! not 100% std compatible

--- lib/xpath_result_set.cpp
+++ lib/xpath_result_set.cpp
@@ -26,8 +26,10 @@ XPathResultSet::XPathResultSet( const XPathResultSet &other ) : query_{other.que
 }
 
 XPathResultSet::XPathResultSet( XPathResultSet &&rhs )
-    : query_{std::move( rhs.query_ )}, resultSet_{rhs.resultSet_}, sz_{rhs.sz_},
-      idx_{rhs.idx_} {}
+    : query_{std::move( rhs.query_ )}
+    , resultSet_{rhs.resultSet_}
+    , sz_{rhs.sz_}
+    , idx_{rhs.idx_} {}
 
 auto XPathResultSet::operator=( const XPathResultSet &rhs ) -> XPathResultSet & {
     if ( this != &rhs ) {
@@ -54,15 +56,92 @@ auto XPathResultSet::operator=( XPathResultSet &&rhs ) -> XPathResultSet & {
 auto XPathResultSet::operator[]( size_t idx ) -> XmlElement {
     if ( resultSet_ && ( idx_ <= sz_ ) ) {
         idx_ = idx;
-        return XmlElement{resultSet_->nodeTab[idx_]};
+        return XmlElement{*( resultSet_->nodeTab + idx_ )};
     }
-    // return XmlNode{nullptr};
+    return XmlElement{};
 }
 
-// private:
-//     XPathQuery query_;
-//     xmlNodeSetPtr resultSet_;
-//     size_t sz_;
-//     size_t idx_;
-// };
+XPathResultSet::iterator::iterator( xmlNodeSet *nodeSet )
+    : set_{nodeSet ? nodeSet->nodeTab : nullptr}
+    , idx_{0}
+    , size_{nodeSet ? nodeSet->nodeNr : 0} {}
+
+auto XPathResultSet::iterator::operator*( ) -> XmlElement {
+    return set_ ? XmlElement{*( set_ + idx_ )} : XmlElement{};
+}
+
+auto XPathResultSet::iterator::operator++( ) -> iterator & {
+
+    ++idx_;
+    if ( size_ == 0 || idx_ == size_ ) {
+        idx_ = 0;
+        set_ = nullptr;
+    }
+    return *this;
+}
+
+auto XPathResultSet::iterator::operator++( int ) -> iterator {
+    iterator tmp = *this;
+    ++idx_;
+    if ( size_ == 0 || idx_ == size_ ) {
+        idx_ = 0;
+        set_ = nullptr;
+    }
+    return tmp;
+}
+
+auto XPathResultSet::iterator::operator--( ) -> iterator & {
+    if ( size_ == 0 || idx_ == 0 ) {
+        set_ = nullptr;
+        return *this;
+    }
+    --idx_;
+    return *this;
+}
+
+auto XPathResultSet::iterator::operator--( int ) -> iterator {
+    if ( size_ == 0 || idx_ == 0 ) {
+        set_ = nullptr;
+        return *this;
+    }
+    iterator tmp = *this;
+    --idx_;
+    return tmp;
+}
+
+auto XPathResultSet::iterator::operator[]( int index ) -> XmlElement {
+    if ( index < 0 ) {
+        idx_ = 0;
+    }
+    if ( index >= size_ ) {
+        idx_ = 0;
+        set_ = nullptr;
+    }
+    idx_ = index;
+    return set_ ? XmlElement{*( set_ + idx_ )} : XmlElement{};
+}
+
+auto XPathResultSet::iterator::operator==( const iterator &other ) const -> bool {
+    return ( set_ == other.set_ ) && ( idx_ == other.idx_ );
+}
+
+auto XPathResultSet::iterator::operator!=( const iterator &other ) const -> bool {
+    return ( set_ != other.set_ ) || ( idx_ != other.idx_ );
+}
+
+auto XPathResultSet::iterator::operator<( const iterator &other ) const -> bool {
+    return ( set_ == other.set_ ) && ( idx_ < other.idx_ );
+}
+
+auto XPathResultSet::iterator::operator>( const iterator &other ) const -> bool {
+    return ( set_ == other.set_ ) && ( idx_ > other.idx_ );
+}
+
+auto XPathResultSet::iterator::operator<=( const iterator &other ) const -> bool {
+    return ( set_ == other.set_ ) && ( idx_ <= other.idx_ );
+}
+
+auto XPathResultSet::iterator::operator>=( const iterator &other ) const -> bool {
+    return ( set_ == other.set_ ) && ( idx_ >= other.idx_ );
+}
 
--- lib/xpath_result_set.hpp
+++ lib/xpath_result_set.hpp
@@ -13,8 +13,32 @@ public:
     XPathResultSet( XPathResultSet &&rhs );
     auto operator=( const XPathResultSet &rhs ) -> XPathResultSet &;
     auto operator=( XPathResultSet &&rhs ) -> XPathResultSet &;
-    auto size( ) const -> size_t { return sz_; }
-    auto operator[]( size_t idx ) -> XmlElement;
+    auto size( ) const -> std::size_t { return sz_; }
+    auto operator[]( std::size_t idx ) -> XmlElement;
+
+    class iterator : public std::iterator<std::random_access_iterator_tag, XmlElement> {
+        xmlNode **set_;
+        std::size_t idx_;
+        int size_;
+
+    public:
+        iterator( xmlNodeSet *nodeSet = nullptr );
+        auto operator*( ) -> XmlElement;
+        auto operator++( ) -> iterator &;
+        auto operator++( int ) -> iterator;
+        auto operator--( ) -> iterator &;
+        auto operator--( int ) -> iterator;
+        auto operator[]( int index ) -> XmlElement;
+        auto operator==( const iterator &other ) const -> bool;
+        auto operator!=( const iterator &other ) const -> bool;
+        auto operator<( const iterator &other ) const -> bool;
+        auto operator>( const iterator &other ) const -> bool;
+        auto operator<=( const iterator &other ) const -> bool;
+        auto operator>=( const iterator &other ) const -> bool;
+    };
+
+    auto begin( ) -> iterator { return iterator{resultSet_}; }
+    auto end( ) -> iterator { return iterator{}; }
 
 private:
     XPathQuery query_;
