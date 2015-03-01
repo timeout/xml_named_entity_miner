871ac816d2cf4e05992f57268940bc38eead8d26
Joe Gain <joe.gain@gmail.com>
2015-03-01 12:29:48 +0100

    XmlElements are now added directly to StackedTextDisplays, which have responsibility for showing the correct display, etc.

--- gui2/stacked_text_display.cpp
+++ gui2/stacked_text_display.cpp
@@ -1,9 +1,16 @@
 #include "stacked_text_display.hpp"
 #include "text_display.hpp"
 
+#include <algorithm>
 #include <iostream>
 
-StackedTextDisplay::StackedTextDisplay( QWidget *parent ) : QStackedWidget{parent} {}
+#include <QDebug>
+
+StackedTextDisplay::StackedTextDisplay( QWidget *parent ) : QStackedWidget{parent} {
+    pos_ = elements_.begin( );
+    emit begin( );
+    emit end( );
+}
 
 void StackedTextDisplay::clear( ) {
     std::cerr << "clearing stacked text display" << std::endl;
@@ -11,49 +18,103 @@ void StackedTextDisplay::clear( ) {
         TextDisplay *display = reinterpret_cast<TextDisplay *>( widget( index ) );
         delete display;
     }
+    setCurrentIndex( -1 );
+    emit begin( );
+    emit end( );
 }
 
-void StackedTextDisplay::insertDisplay( int index, const QString &text ) {
-    TextDisplay *display = new TextDisplay( text );
-    // connect( display, &TextDisplay::entrySelected, &StackedTextDisplay::addDisplayIndex);
-    insertWidget( index, display );
-}
-
-void StackedTextDisplay::removeDisplay( int index ) {
-    std::cerr << "removing display: " << index << std::endl;
-    TextDisplay *display = reinterpret_cast<TextDisplay *>( widget( index ) );
-    delete display;
-    std::cerr << "display count: " << count( ) << std::endl;
-}
-
-/// adds ontology at index index to all displays
-void StackedTextDisplay::addOntology( const QColor &color ) {
-    for ( int index = 0; index < count( ); ++index ) {
-        TextDisplay *display = reinterpret_cast<TextDisplay *>( widget( index ) );
-        display->addOntology( color );
+void StackedTextDisplay::addElement( const XmlElement &element ) {
+    // only add elements once
+    auto search = std::find( std::begin( elements_ ), std::end( elements_ ), element );
+    if ( search != std::end( elements_ ) ) {
+        return;
     }
-}
 
-void StackedTextDisplay::removeOntology( int ontologyIndex ) {
-    for ( int index = 0; index < count( ); ++index ) {
-        TextDisplay *display = reinterpret_cast<TextDisplay *>( widget( index ) );
-        display->removeOntology( ontologyIndex );
+    auto iter = std::begin( elements_ );
+    while ( iter != std::end( elements_ ) && *iter < element ) {
+        ++iter;
     }
+    pos_ = elements_.insert( iter, element );
+
+    // create TextDisplay
+    auto text = QString::fromStdString( element.content( ) );
+    TextDisplay *textDisplay = new TextDisplay{text, this};
+    // insert
+    auto index = std::distance( std::begin( elements_ ), pos_ );
+    insertWidget( index, textDisplay );
+
+    setCurrentIndex( index );
+    emit currentChanged( index );
+    beginEnd( );
 }
 
-/// add rule to widget at index  to ontology rule set at index ontologyIndex
-void StackedTextDisplay::insertHighlightRule( int index, int ontologyIndex,
-                                              const QString &rule ) {
-    TextDisplay *display = reinterpret_cast<TextDisplay *>( widget( index ) );
-    display->addHighlightRule( ontologyIndex, rule );
+void StackedTextDisplay::removeElement( const XmlElement &element ) {
+    auto search = std::find( std::begin( elements_ ), std::end( elements_ ), element );
+    if ( search == std::end( elements_ ) ) {
+        return;
+    }
+    auto index = std::distance( std::begin( elements_ ), search );
+    TextDisplay *textDisplay = reinterpret_cast<TextDisplay *>( widget( index ) );
+    delete textDisplay;
+    elements_.erase( search );
+    emit widgetRemoved( index );
+    if ( count( ) == 0 ) { // there _was_ only one display
+        setCurrentIndex( -1 );
+        pos_ = std::end( elements_ );
+    } else if ( index == 0 ) { // removed first display
+        setCurrentIndex( 0 );
+        pos_ = std::begin( elements_ );
+    } else if ( index == count( ) ) { // removed last display
+        qDebug( ) << "deleting last display";
+        setCurrentIndex( --index );
+        pos_ = std::prev(std::end( elements_ ), 1);
+    } else {
+        setCurrentIndex( index );
+    }
+    beginEnd( );
+    emit currentChanged( index );
 }
 
-void StackedTextDisplay::removeHighlightRule( int index, int ontologyIndex,
-                                              const QString &rule ) {
-    TextDisplay *display = reinterpret_cast<TextDisplay *>( widget( index ) );
-    display->removeHighlightRule( ontologyIndex, rule );
+void StackedTextDisplay::addOntology( const QString &ontologyName,
+                                      const QColor formatColor ) {
+    ontologyMap_.emplace( ontologyName, formatColor );
 }
 
-void StackedTextDisplay::addDisplayIndex( const QString &entry ) {
-    
+void StackedTextDisplay::removeOntology( const QString &ontologyName ) {
+    ontologyMap_.erase( ontologyName );
+    // TODO:
+    // remove ontologies in TextDisplay and TextDisplayHighlighter
+}
+
+void StackedTextDisplay::highlight( const QString &ontologyName, const QString &entiy ) {}
+
+void StackedTextDisplay::next( ) {
+    auto it = pos_;
+    if ( std::next( it ) != std::end( elements_ ) ) {
+        ++pos_;
+    }
+    beginEnd( );
+    auto index = std::distance( std::begin( elements_ ), pos_ );
+    setCurrentIndex( index );
+    emit currentChanged( index );
+}
+
+void StackedTextDisplay::prev( ) {
+    if ( pos_ != std::begin( elements_ ) ) {
+        --pos_;
+    }
+    beginEnd( );
+    auto index = std::distance( std::begin( elements_ ), pos_ );
+    setCurrentIndex( index );
+    emit currentChanged( index );
+}
+
+auto StackedTextDisplay::beginEnd( ) -> void {
+    if ( pos_ == std::begin( elements_ ) ) {
+        emit begin( );
+    }
+    auto it = pos_;
+    if ( std::next( it ) == std::end( elements_ ) ) {
+        emit end( );
+    }
 }
--- gui2/stacked_text_display.hpp
+++ gui2/stacked_text_display.hpp
@@ -1,6 +1,12 @@
 #pragma once
+#include "xml_element.hpp"
+#include <list>
+#include <map>
+#include <QColor>
 #include <QStackedWidget>
-#include <QMap>
+#include <QString>
+
+class QWidget;
 
 class StackedTextDisplay : public QStackedWidget {
     Q_OBJECT
@@ -9,16 +15,22 @@ public:
 
 public slots:
     void clear( );
-    void insertDisplay( int index, const QString &text );
-    void removeDisplay( int index );
-    void addOntology( const QColor &color );
-    void removeOntology( int ontologyIndex );
-    void insertHighlightRule( int index, int ontologyIndex, const QString &rule );
-    void removeHighlightRule( int index, int ontologyIndex, const QString &rule );
+    void addElement( const XmlElement &element );
+    void removeElement( const XmlElement &element );
+    void addOntology( const QString &ontologyName, const QColor formatColor );
+    void removeOntology( const QString &ontologyName );
+    void highlight( const QString &ontologyName, const QString &entiy );
+    void next( );
+    void prev( );
 
 signals:
-    void entrySelected( int index, const QString &entry );
+    void entitySelected( const QString &entity );
+    void end( );
+    void begin( );
 
-private slots:
-    void addDisplayIndex( const QString &entry );
+private:
+    auto beginEnd( ) -> void;
+    std::list<XmlElement> elements_;
+    std::list<XmlElement>::iterator pos_;
+    std::map<QString, QColor> ontologyMap_;
 };
