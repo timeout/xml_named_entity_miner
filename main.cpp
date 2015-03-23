ed70ef61a5a8db9015454a6169ed24a2f1ca54eb
Joe Gain <joe.gain@gmail.com>
2015-03-23 14:43:21 +0100

    removed commented out old Mainwindow class

--- main.cpp
+++ main.cpp
@@ -5,7 +5,6 @@ int main( int argc, char *argv[] ) {
     QApplication app( argc, argv );
     app.setOrganizationName("University-of-Konstanz");
     app.setApplicationName("Named-Entity-Miner");
-    // Mainwindow win;
     MainWindow win;
     win.show( );
     return app.exec( );
